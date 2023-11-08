bb269719bed87beda37a7906fd4102e12e3a31a9
korawit <korawit.s@robolingo.ai>
2023-11-08 21:11:26 +0700

    update

--- controllers/blessing/createBlessing.js
+++ controllers/blessing/createBlessing.js
@@ -1,7 +1,10 @@
 const moment = require("moment")
 const { blessingModel } = require('../../models')
 const {lineNoti} = require('../linenoti')
-const { error } = require("jquery")
+const { ref, uploadString } = require("firebase/storage");
+const storage = require("../../lib/firebase")
+const { v4: uuidV4 } = require('uuid');
+const { urlencoded } = require("body-parser");
 module.exports = class createBlessing {
     constructor() {
       this.date = new Date()
@@ -10,12 +13,26 @@ module.exports = class createBlessing {
         try {
             const body = req.body
 
-            const data = {
+            let data = {
                 name:  (body?.name && body?.name != '') ? body?.name : `testName_${moment(this.date).format("YYYY-MM-DD hh:mm:ss")}` ,
                 blessing: body?.blessing ?? null,
-                image: body?.image ?? null,
                 date: moment(this.date).format("YYYY-MM-DD")
             }
+            
+            if(body?.image){
+                const fileName = `${data.name}_${uuidV4()}`
+                const imageRef = ref(storage, fileName)
+                
+                uploadString(imageRef, (body?.image), 'data_url').then((snapshot) => {
+                    console.log(`Upload Success ${imageRef}`);
+                });
+
+                data = {
+                    ...data,
+                    image: encodeURI(`https://firebasestorage.googleapis.com/v0/b/${process.env.FIREBASE_BUCKET}/o/${fileName}?alt=media`)
+                }
+            }
+
             await blessingModel.create(data).then(() => {
                 res.status(200).json({ data: data, date: data?.date })
                 lineNoti(data)
@@ -23,7 +40,7 @@ module.exports = class createBlessing {
             }).catch((error) => {
                 //
                 console.error('[CREATE] blessing error: ', error)
-                return res.status(400).json({ msg: error})
+                return res.status(400).json({ msg: error })
             })
         } catch (error) {
             console.error('[CREATE] blessing error: ', error)
--- controllers/linenoti.js
+++ controllers/linenoti.js
@@ -1,26 +1,52 @@
 const axios = require('axios');
 const qs = require('qs')
 const token = process.env.LINETOKEN
+const request = require('request') 
 module.exports = {
     lineNoti: async(data) => {
         try {
-            await axios({
-                method: "post",
-                url: "https://notify-api.line.me/api/notify",
-                headers: {
-                  "Content-Type": "application/x-www-form-urlencoded",
-                  "Authorization": `Bearer ${token}`,
-                },
-                data: qs.stringify({
-                    message: `\n####----####\nname: ${data.name} \nblessing: ${data.blessing}\n####----####`,
-                  })
-                })
-                .then((response) => {
-                  //--//
-                })
-                .catch((err) => {
-                  console.log(err);
-                });
+          let msg = {
+            message: `\n####----####\n\nName: ${data?.name} \n\nblessing: ${data?.blessing}\n\n####----####`
+          }
+          if(data?.image){
+            msg = {
+              ...msg,
+              imageThumbnail: data?.image,
+              imageFullsize: data?.image
+            }
+          }
+          request({
+            method: 'POST',
+            uri: `https://notify-api.line.me/api/notify`,
+            header: {
+                'Content-Type': 'multipart/form-data',
+            },
+            auth: {
+                bearer: `${token}`,
+            },
+            form: msg,
+        }, (err, httpResponse, body) => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log(body)
+            }
+        });
+          // await axios({
+          //     method: "post",
+          //     url: "https://notify-api.line.me/api/notify",
+          //     headers: {
+          //       'Content-Type': 'multipart/form-data',
+          //       "Authorization": `Bearer ${token}`,
+          //     },
+          //     form: msg
+          //   })
+          //   .then((response) => {
+          //     //--//
+          //   })
+          //   .catch((err) => {
+          //     console.log(err);
+          //   });
         } catch (error) {
             console.log(error)
         }
